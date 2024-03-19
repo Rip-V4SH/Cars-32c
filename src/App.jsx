6ad0ae5e4b97f9df2271e6a76d3126d2ae703e00
Rip-V4SH <tioban.alm@gmail.com>
2024-03-18 23:37:34 -0300

    segundo commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import Sedans from "./assets/icon-sedans.svg"
+import Suv from "./assets/icon-suvs.svg"
+import Luxo from "./assets/icon-luxury.svg"
+
 export default function App() {
   return (
-    <h1>Hello world</h1>
+    <main>
+      <section>
+        <div>
+          <img src={Sedans} alt="" />
+          <h2>Sedans</h2>
+          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank">ver mais</a>
+        </div>
+
+        <div>
+          <img src={Suv} alt="" />
+          <h2>SUVs</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/suvs" target="_blank">ver mais</a>
+        </div>
+
+        <div>
+          <img src={Luxo} alt="" />
+          <h2>Luxuosos</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">ver mais</a>
+        </div>
+      </section>
+    </main>
   )
 }
